5f0ee0965293030990a725c60428f5d73db720a3
dbtop <berdaddario@gmail.com>
2022-11-18 18:43:07 -0300

    organizing code

--- components/cart/Cart.tsx
+++ components/cart/Cart.tsx
@@ -57,30 +57,78 @@ export const Cart: React.FC<CartProps> = ({ products, total }) => {
             )
         , [cart]
     )
-    
-    const clearCart = () : void => {
+
+    const clearCart = (): void => {
         setCart([])
     }
 
     return (
         <>
-            <Flex justifyContent='flex-end' maxWidth={'100px'} alignSelf='end' padding={5} cursor='pointer'  >
-                <Image onClick={onOpen} src="/cart.svg" alt='cart' width={30} height={30} />
+            <Flex
+                justifyContent='flex-end'
+                maxWidth={'100px'}
+                alignSelf='end'
+                padding={5}
+                cursor='pointer'
+            >
+                <Image
+                    onClick={onOpen}
+                    src="/cart.svg"
+                    alt='cart'
+                    width={30}
+                    height={30} />
             </Flex>
 
-            <Drawer placement={'left'} onClose={onClose} isOpen={isOpen}>
+            <Drawer
+                placement={'left'}
+                onClose={onClose}
+                isOpen={isOpen}
+            >
                 <DrawerOverlay />
                 <DrawerContent>
-                    <DrawerHeader backgroundColor='gray.800' borderBottomWidth='1px' color='white'>Shop Cart</DrawerHeader>
-                    <DrawerBody backgroundColor='gray.800' >
+                    <DrawerHeader
+                        backgroundColor='gray.800'
+                        borderBottomWidth='1px'
+                        color='white'>Shop Cart
+                    </DrawerHeader>
+                    <DrawerBody
+                        backgroundColor='gray.800' >
                         {
                             cart.map((product: Product) => (
-                                <Flex key={product.id} gap={2} textAlign='left' justifyContent='space-between' alignItems='center' marginY={4} padding={0} >
-                                    <Image src={product.images[0]} alt={product.title} width={50} height={50} />
-                                    <Text color='white'  >{product.title}</Text>
-                                    <Text color='white' >{currencyParser(product.price)}</Text>
-                                    <Button onClick={() => deleteHandler(Number(product.id))} color='white' borderRadius='9999' marginLeft={3} colorScheme={'red'} paddingX={2} fontSize='2xs'>
-                                        <Image src="/delete.svg" alt='trash' width={15} height={15} />
+                                <Flex
+                                    key={product.id}
+                                    gap={2}
+                                    textAlign='left'
+                                    justifyContent='space-between'
+                                    alignItems='center'
+                                    marginY={4}
+                                    padding={0}
+                                >
+                                    <Image
+                                        src={product.images[0]}
+                                        alt={product.title}
+                                        width={50}
+                                        height={50} />
+                                    <Text
+                                        color='white'>{product.title}
+                                    </Text>
+                                    <Text
+                                        color='white'>{currencyParser(product.price)}
+                                    </Text>
+                                    <Button
+                                        onClick={() => deleteHandler(Number(product.id))}
+                                        color='white'
+                                        borderRadius='9999'
+                                        marginLeft={3}
+                                        colorScheme={'red'}
+                                        paddingX={2}
+                                        fontSize='2xs'>
+                                        <Image
+                                            src="/delete.svg"
+                                            alt='trash'
+                                            width={15}
+                                            height={15}
+                                        />
                                     </Button>
                                 </Flex>
                             ))
@@ -88,20 +136,69 @@ export const Cart: React.FC<CartProps> = ({ products, total }) => {
                         {
                             Boolean(cart.length > 0) ? (
                                 <>
-                                    <Flex width='100%' justify='space-between' marginTop={4} paddingX={8} borderTop='1px solid white'>
-                                        <Text color='white' textAlign='right' margin={2} fontWeight="semi-bold" paddingY={3}>Total:</Text>
-                                        <Text color='white' textAlign='right' margin={2} fontWeight="bold" paddingY={3}> {totalCart} </Text>
+                                    <Flex
+                                        width='100%'
+                                        justify='space-between'
+                                        marginTop={4}
+                                        paddingX={8}
+                                        borderTop='1px solid white'
+                                    >
+                                        <Text
+                                            color='white'
+                                            textAlign='right'
+                                            margin={2}
+                                            fontWeight="semi-bold"
+                                            paddingY={3}
+                                        >Total:
+                                        </Text>
+                                        <Text
+                                            color='white'
+                                            textAlign='right'
+                                            margin={2}
+                                            fontWeight="bold"
+                                            paddingY={3}> {totalCart}
+                                        </Text>
                                     </Flex>
-                                    <Flex alignSelf='center' margin={2} gap={4} padding={2}>
-                                        <Button as={Link} padding={'3'}
-                                             _hover={{ bg: 'blue.300' }} width={300} target="_blank" textAlign='center' color='white' fontWeight='bold' href={`https://wa.me/3546650023?text=${encodeURIComponent(text)}`}  marginX='auto' colorScheme='whatsapp' > Check out
+                                    <Flex
+                                        alignSelf='center'
+                                        margin={2}
+                                        gap={4}
+                                        padding={2}
+                                    >
+                                        <Button
+                                            as={Link}
+                                            padding={'3'}
+                                            _hover={{ bg: 'blue.300' }}
+                                            width={300}
+                                            target="_blank"
+                                            textAlign='center'
+                                            color='white'
+                                            fontWeight='bold'
+                                            href={`https://wa.me/3546650023?text=${encodeURIComponent(text)}`}
+                                            colorScheme='whatsapp' >
+                                            Check out
                                         </Button>
-                                        <Button padding={'3'}
-                                             _hover={{ bg: 'gray.600' }} width={300}  textAlign='center' color='white' fontWeight='bold' variant='outline' onClick={clearCart}> Clear Cart
+                                        <Button
+                                            padding={'3'}
+                                            _hover={{ bg: 'gray.600' }}
+                                            width={300}
+                                            textAlign='center'
+                                            color='white'
+                                            fontWeight='bold'
+                                            variant='outline'
+                                            onClick={clearCart}
+                                        > Clear Cart
                                         </Button>
                                     </Flex>
                                 </>
-                            ) : <Text color='white' textAlign='center' margin={2} fontWeight="bold" paddingY={3}>Cart is empty</Text>
+                            ) : <Text
+                                color='white'
+                                textAlign='center'
+                                margin={2}
+                                fontWeight="bold"
+                                paddingY={3}
+                            >Cart is empty
+                            </Text>
                         }
                     </DrawerBody>
                 </DrawerContent>
--- pages/index.tsx
+++ pages/index.tsx
@@ -34,23 +34,47 @@ const IndexRoute: React.FC<Props> = ({ products }) => {
         total = {currencyParser(cart.reduce((total, product) => Number(total + product.price), 0))}
       />
 
-      <Grid gridGap={5} justifyItems='center' templateColumns='repeat(auto-fill, minmax(200px,1fr))'>
+      <Grid 
+        gridGap={5} 
+        justifyItems='center' 
+        templateColumns='repeat(auto-fill, minmax(200px,1fr))'>
         {
           products.products.map((product: Product) => (
-            <Stack backgroundColor='gray.700' spacing={3} borderRadius={10} padding={4} key={product.id}>
+            <Stack 
+              key={product.id}
+              backgroundColor='gray.700' 
+              spacing={3} 
+              borderRadius={10} 
+              padding={4} 
+            >
 
               <Stack spacing='1'>
-                <Text color='white' fontSize='xl' textAlign="center" fontWeight="light" >{product.title}</Text>
+                <Text 
+                  color='white' 
+                  fontSize='xl' 
+                  textAlign="center" 
+                  fontWeight="light" >{product.title}
+                </Text>
               </Stack>
 
               <Stack alignItems='center'>
-                <Image src={product.images[0]} style={{objectFit:'cover', maxHeight:'128px'}}  alt={product.title} width={200} height={200} />
-                <Text color='white' textAlign='left'>{product.description}</Text>
-                <Text color='white' textAlign='center' fontWeight="bold">{currencyParser(product.price)}</Text>
+                <Image 
+                  src={product.images[0]} 
+                  style={{objectFit:'cover', maxHeight:'128px'}}  
+                  alt={product.title} 
+                  width={200} 
+                  height={200} />
+                <Text 
+                  color='white' 
+                  textAlign='left'>{product.description}
+                </Text>
+                <Text 
+                  color='white' 
+                  textAlign='center' 
+                  fontWeight="bold">{currencyParser(product.price)}
+                </Text>
               </Stack>
-
               <Stack>
-
                 <Button
                   colorScheme='primary'
                   fontSize='sm'
@@ -59,25 +83,15 @@ const IndexRoute: React.FC<Props> = ({ products }) => {
                   size='sm'
                   _hover={{ bg: 'primary.500' }}
                   variant='outline'
-                >
+              >
                   Add to Cart
                 </Button>
               </Stack>
-
             </Stack>
           ))
 
         }
       </Grid>
-      {/* 
-        {
-          Boolean(cart.length) &&
-            <Button as={Link} padding={'6'}
-              position='sticky' _hover={{ bg: 'teal'}} target="_blank" textAlign='center'  color='white' fontWeight='bold' href={`https://wa.me/3546650023?text=${encodeURIComponent(text)}`} width={300} marginX='auto' colorScheme='whatsapp' > Check out  {cart.length} items in cart
-            </Button>
-        }
-      </Box> */}
-
     </Stack>
   );
 }
